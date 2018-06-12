bfb931626cb3ade15c3bcfca917778547dd15cc7
aalaniz18 <a.alanizf04@gmail.com>
2018-06-12 18:55:26 -0300

    modifico Administrador

--- src/main/resources/Administrador.java
+++ src/main/resources/Administrador.java
@@ -3,12 +3,39 @@ package resources;
 public class Administrador {
 	private String usuario;
 	private String contrasena;
-	//private String nombre;
+	private String nombre;
+	private static int contadorInstancia;
+	private static Administrador instancia;
 	
 	public Administrador(String u, String c, String n){
 		usuario=u;
 		contrasena=c;
-		//nombre=n;
+		nombre=n;
+		contadorInstancia=0;
+	}
+	
+	public Administrador(){
+		contadorInstancia=0;
+	}
+	
+	public static void crearInstancia(){
+		if(instancia==null){
+			instancia= new Administrador();
+		}
+	}
+	
+	public static Administrador getInstancia(){
+		contadorInstancia++;
+		if(instancia==null){
+			crearInstancia();
+		}
+		return instancia;
+	}
+	
+	public void setUsuario(String a, String b, String c){
+		usuario=a;
+		contrasena=b;
+		nombre=c;
 	}
 	
 	public void setPass(String c){
